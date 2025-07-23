23700bcaeee933bbfa6ff1448751e5bdc332b581
Banu Vinay <banu@virtualbuddy.in>
2025-07-23 17:01:46 +0530

    Fix: Use full ARM path for ADLS registration to avoid NotFound error

--- scripts/adls_scan.sh
+++ scripts/adls_scan.sh
@@ -22,7 +22,7 @@ ARM_RESOURCE_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GRO
 echo "📌 Registering ADLS as a data source in Purview..."
 
 az rest --method put \
-  --uri "https://${PURVIEW_NAME}.purview.azure.com/scanning/datasources/${STORAGE_ACCOUNT_NAME}?api-version=2022-09-01-preview" \
+  --uri "https://${PURVIEW_NAME}.purview.azure.com/scanning/datasources/${ARM_RESOURCE_ID}?api-version=2022-09-01-preview" \
   --headers "Content-Type=application/json" \
   --resource "https://purview.azure.net" \
   --body @- <<EOF
