176a4463d324c23f4b359021b6832161231d272a
Banu Vinay <banu@virtualbuddy.in>
2025-07-23 17:15:35 +0530

    Fix: use storage account name in Purview scan registration URI

--- scripts/adls_scan.sh
+++ scripts/adls_scan.sh
@@ -10,7 +10,7 @@ az version
 # Variables
 PURVIEW_NAME="banupurview"
 SCAN_NAME="automated_adls_scan1"
-COLLECTION_NAME="banupurview"  # Or "default" if that's your collection
+COLLECTION_NAME="banupurview"
 RESOURCE_GROUP="purviewproject"
 SUBSCRIPTION_ID="e34ac57d-3802-4c72-9bf9-67b23f939b24"
 STORAGE_ACCOUNT_NAME="pvadls1ixtg6uo5qrq4e"
@@ -18,11 +18,12 @@ CREDENTIAL_NAME="ADLS_Raw"
 SCAN_RULE_SET_NAME="System-DefaultAzureStorage"
 
 ARM_RESOURCE_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Storage/storageAccounts/${STORAGE_ACCOUNT_NAME}"
+RESOURCE_PATH="${ARM_RESOURCE_ID:1}"  # remove leading slash for URI
 
 echo "📌 Registering ADLS as a data source in Purview..."
 
 az rest --method put \
-  --uri "https://${PURVIEW_NAME}.scan.purview.azure.com/scanning/datasources/${STORAGE_ACCOUNT_NAME}?api-version=2022-12-01-preview" \
+  --uri "https://${PURVIEW_NAME}.scan.purview.azure.com/scanning/datasources/${RESOURCE_PATH}?api-version=2022-12-01-preview" \
   --headers "Content-Type=application/json" \
   --resource "https://purview.azure.net" \
   --body @- <<EOF
